8fde592aac895f92fefd466b452401085769f2fc
Mars-Rover-Manipal <electronics.marsrovermanipal@gmail.com>
2020-02-21 22:50:04 +0530

    .

--- rhinoceROS/build/rover_mrm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leander/rhinoceROS/src/rover_mrm
-# Build directory: /home/leander/rhinoceROS/build/rover_mrm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- rhinoceROS/build/rover_sensors_urdf/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leander/rhinoceROS/src/rover_sensors_urdf/src
-# Build directory: /home/leander/rhinoceROS/build/rover_sensors_urdf/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
